a91bcf950163c3066138006be0026dd504b1e608
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-01-09 00:08:20 +0200

    bad fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import SearchBar from './SearchBar/Searchbar';
 import Modal from './Modal/Modal';
 import fetchImage from './FetchImage/FetchImage';
@@ -8,103 +8,177 @@ import { Button } from './Button/Button';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    // index: null,
-    page: 1,
-    loading: false,
-    showModal: false,
-    // error: null,
-    modalImage: '',
-    imageAlt: '',
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  const [imageAlt, setImageAlt] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.setState({ loading: true });
+  useEffect(() => {
+    // query === ''
+    if (!query) {
+      setLoading(true);
 
-      fetchImage(this.state.query, this.state.page)
+      fetchImage(query, page)
         .then(response => {
           if (response.ok) {
             return response.json();
           }
         })
-        .then(images => this.setState({ images: images.hits }))
-        .catch(error => this.setState({ error }))
+        .then(images => {                if (page === 1) {          setImages([...images.hits])          return;        }        setImages(prev => [...prev, ...images.hits]);      })
+        // .then(images => setImages(images.hits))
+        .catch(error => console.log(error))
         .finally(() => {
-          this.setState({ loading: false });
-          this.setState(prevState => ({ page: prevState.page + 1 }));
+          setLoading(false);
+          setPage(page + 1);
         });
     }
-  }
+  }, [query, page]);
 
-  handleFormSubmit = query => {
-    if (this.state.query === query) {
+  const handleFormSubmit = query => {
+    if (query === query) {
       toast.error('You enter the same word!🦄 Enter new one!', {
         theme: 'colored',
       });
     }
-    this.setState({
-      query,
-      page: 1,
-      images: [],
-    });
+
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  // loadMore = () => {
-  //   this.setState({ loading: true });
-  //   this.setState(prevState => ({ page: prevState.page + 1 }));
-  // };
-
-  loadMoreImages = () => {
-    this.setState({ loading: true });
-    fetchImage(this.state.query, this.state.page)
+  const loadMoreImages = () => {
+    setLoading(true);
+    fetchImage(query, page)
       .then(response => {
         if (response.ok) {
           return response.json();
         }
       })
-      .then(images =>
-        this.setState({ images: [...this.state.images, ...images.hits] })
-      )
-      .catch(error => this.setState({ error }))
+      .then(images => setImages([...images, ...images.hits]))
+      .catch(error => console.log(error))
       .finally(() => {
-        this.setState({ loading: false });
-        this.setState(prevState => ({ page: prevState.page + 1 }));
+        setLoading(false);
+        setPage(page + 1);
       });
   };
 
-  toggleModal = (modalImage, imageAlt) => {
-    this.setState(state => ({
-      showModal: !state.showModal,
-      modalImage,
-      imageAlt,
-    }));
+  const toggleModal = (modalImage, imageAlt) => {
+    setShowModal(prevShowModal => !prevShowModal);
+    setModalImage(modalImage);
+    setImageAlt(imageAlt);
   };
 
-  render() {
-    const { showModal, loading, images, modalImage, imageAlt } = this.state;
-    return (
-      <div className={css.App}>
-        {/* {error && alert(`{error.massage}`)} */}
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery openModal={this.toggleModal} images={images} />
-        <ToastContainer
-          position="top-center"
-          autoClose={4000}
-          theme="colored"
-        />
-        {loading && <Loader />}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={modalImage} alt={imageAlt} />
-          </Modal>
-        )}
-        {images.length >= 12 && <Button onClick={this.loadMoreImages} />}
-      </div>
-    );
-  }
+  return (
+    <div className={css.App}>
+      <SearchBar onSubmit={handleFormSubmit} />
+      <ImageGallery openModal={toggleModal} images={images} />
+      <ToastContainer position="top-center" autoClose={4000} theme="colored" />
+      {loading && <Loader />}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={modalImage} alt={imageAlt} />
+        </Modal>
+      )}
+      {images.length >= 12 && <Button onClick={loadMoreImages} />}
+    </div>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     images: [],
+//     page: 1,
+//     loading: false,
+//     showModal: false,
+//     modalImage: '',
+//     imageAlt: '',
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.query !== this.state.query) {
+//       this.setState({ loading: true });
+
+//       fetchImage(this.state.query, this.state.page)
+//         .then(response => {
+//           if (response.ok) {
+//             return response.json();
+//           }
+//         })
+//         .then(images => this.setState({ images: images.hits }))
+//         .catch(error => this.setState({ error }))
+//         .finally(() => {
+//           this.setState({ loading: false });
+//           this.setState(prevState => ({ page: prevState.page + 1 }));
+//         });
+//     }
+//   }
+
+//   handleFormSubmit = query => {
+//     if (this.state.query === query) {
+//       toast.error('You enter the same word!🦄 Enter new one!', {
+//         theme: 'colored',
+//       });
+//     }
+//     this.setState({
+//       query,
+//       page: 1,
+//       images: [],
+//     });
+//   };
+
+//   loadMoreImages = () => {
+//     this.setState({ loading: true });
+//     fetchImage(this.state.query, this.state.page)
+//       .then(response => {
+//         if (response.ok) {
+//           return response.json();
+//         }
+//       })
+//       .then(images =>
+//         this.setState({ images: [...this.state.images, ...images.hits] })
+//       )
+//       .catch(error => this.setState({ error }))
+//       .finally(() => {
+//         this.setState({ loading: false });
+//         this.setState(prevState => ({ page: prevState.page + 1 }));
+//       });
+//   };
+
+//   toggleModal = (modalImage, imageAlt) => {
+//     this.setState(state => ({
+//       showModal: !state.showModal,
+//       modalImage,
+//       imageAlt,
+//     }));
+//   };
+
+//   render() {
+//     const { showModal, loading, images, modalImage, imageAlt } = this.state;
+//     return (
+//       <div className={css.App}>
+//         {/* {error && alert(`{error.massage}`)} */}
+//         <SearchBar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery openModal={this.toggleModal} images={images} />
+//         <ToastContainer
+//           position="top-center"
+//           autoClose={4000}
+//           theme="colored"
+//         />
+//         {loading && <Loader />}
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <img src={modalImage} alt={imageAlt} />
+//           </Modal>
+//         )}
+//         {images.length >= 12 && <Button onClick={this.loadMoreImages} />}
+//       </div>
+//     );
+//   }
+// }
